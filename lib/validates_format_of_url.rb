eb6c6ce1a3af72cb3c74ffbc238a57d3a57d8e02
n0ts <naoya.n@gmail.com>
2010-05-18 20:20:27 +0900

    Fixed a encode bug

--- lib/validates_format_of_url.rb
+++ lib/validates_format_of_url.rb
@@ -2,7 +2,7 @@ module ValidatesFormatOfUrl
   def self.validates(url, allowed_schemes = ['http', 'https'])
     require 'uri/http'
     begin
-      uri = URI.parse(url)
+      uri = URI.parse(URI.encode(url))
       if !allowed_schemes.include?(uri.scheme)
         raise URI::InvalidURIError
       end
